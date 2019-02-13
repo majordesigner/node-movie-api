4679cc86a7eadb73c062fbc0ded56649c34dff5b
hasan.gocmen <hasan.gocmen@morhipo.com>
2019-02-13 17:23:19 +0300

    json web token verify

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const app = express();
 // db connection
 const db = require('./helper/db.js')();
 
+// Config file
+const config = require('./config');
+app.set('api_secret_key', config.api_secret_key);
+
+// Middleware
+const verifyToken = require('./middleware/verify-token');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movies', movieRouter);
 app.use('/api/directors', directorRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router = express.Router();
 
+// parola kullanma durumlarında şifreyi hashlemek için kullanılır. Şifrelemek için hash şifreyi geri çözümlemek için compare methodu kullanılır.
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+//Model
+const User = require('../models/User');
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', (req, res, next) => {
+  const {username, password} = req.body;
+
+  bcrypt.hash(password, 10).then((hash) => {
+    const user = new User({
+      username,
+      password: hash
+    });
+  
+    const promise = user.save();
+  
+    promise.then((data) => {
+      res.json(data);
+    }).catch((err) => {
+      res.json(err);
+    });
+  });
+});
+
+
+router.post('/authenticate', (req, res) => {
+  const {username, password} = req.body;
+
+  User.findOne({
+    username
+  }, (err, user) => {
+    if(err)
+      throw err;
+
+    if(!user) {
+      res.json({
+        status: false,
+        message: 'Authentication failed, User not found.'
+      });
+    }
+    else {
+      bcrypt.compare(password, user.password).then((result) => {
+        if(!result) {
+          res.json({
+            status:false,
+            message: 'Authentication failed, Wrong password.'
+          });
+        }
+        else {
+          const payload = {
+            username
+          }
+
+          const token = jwt.sign(payload, req.app.get('api_secret_key'), {
+            expiresIn: 720 // dakika cinsindendir. 12 saat eder.
+          });
+
+          res.json({
+            status: true,
+            token
+          });
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
