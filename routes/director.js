49ec3d2f646b421bad94b516d45d285b622b52ba
hasan.gocmen <hasan.gocmen@morhipo.com>
2019-02-13 15:15:02 +0300

    api/directors işlemleri

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -16,7 +17,103 @@ router.post('/', (req, res) => {
   });
 });
 
+// Tüm directorların listesi döndürülür ama Film listesinde olan filmleri de eklenerek. Join işlemleri
+router.get('/', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true // Joine dahil olamayan filmi olmayan yönetmenleri de listede görebilmek için kullanılır.
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        }, // Director bilgisi için yeni bi obje düzenliyor
+        movies: {
+          $push: '$movies'
+        } // movieler için oluşturulan kayıtları da bu obje altında toparlıyor.
+      }
+    },
+    { // group ile oluşan yonetmen bilgisini tutan objeden kurtulmak için böyle bişey yapılıyor.
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+  ]);
 
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
 
+// İd bilgisi ile belli bir yonetmene ait datalar çekilir. Üsttekinden tek farkı aggregate içerisine $match keywordu ekledik.
+router.get('/:director_id', (req, res) => {
+  const promise = Director.aggregate([
+    {
+      $match: {
+        '_id': mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$movies',
+        preserveNullAndEmptyArrays: true // Joine dahil olamayan filmi olmayan yönetmenleri de listede görebilmek için kullanılır.
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        }, // Director bilgisi için yeni bi obje düzenliyor
+        movies: {
+          $push: '$movies'
+        } // movieler için oluşturulan kayıtları da bu obje altında toparlıyor.
+      }
+    },
+    { // group ile oluşan yonetmen bilgisini tutan objeden kurtulmak için böyle bişey yapılıyor.
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+  ]);
+
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });  
+});
 
 module.exports = router;
